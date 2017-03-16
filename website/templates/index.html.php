8b2a9d33197e1f99c1ed43e7dbfab69266800b10
chaochiai <chantalochiaiit@gmail.com>
2017-03-16 08:43:04 +0100

    bootstrap

---
+++ website/templates/index.html.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>Log in Form</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
+<body>
+	<h1>Log in</h1>
+	<div class="row">
+		<div class="col-xs-1 col-sm-6 col-lg-4">
+			<h2>Content 1</h2>
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			v
+			v
+		</div>
+		<div class="col-xs-1 col-sm-6 col-lg-4">
+			<h2>Content 2</h2>
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			v
+			v
+		</div>
+		<div class="col-xs-1 col-sm-6 col-lg-4">
+			<h2>Content 3</h2>
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			blablablabla blablabl abl blabal
+			v
+			v
+		</div>
+	</div>
+	<button class="btn btn-success">My Value</button>
+	
+</body>
+</html>
