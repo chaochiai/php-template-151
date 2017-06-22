da1e086fbf8c94f3a329fd8761f6ff3f040dd5b0
chaochiai <chantalochiaiit@gmail.com>
2017-06-22 20:24:34 +0200

    home page done

--- website/templates/index.html.php
+++ website/templates/index.html.php
@@ -10,21 +10,10 @@
 			 The user can enter their food intake and weight every day.  With doing so, the user will have the overview of the diet.
 		</div>
 		<div class="col-xs-1 col-sm-6 col-lg-10">
-			<h2>Use Case</h2>
-			<div>
-				1.	The user creates an account by entering the user’s name, account credentials, weight, goal, goal weight, calories intake per day, etc. 
-			</div>
-			<div>2.	After that, the user can sign in.</div>
-			<div>3.	The application will show the page “Your Journey”, which shows the progress of the diet.</div>
-			<div>4.	The user enters it’s daily entries on the “Today” page. The user also enters the weight.</div>
-			<div>5.	The user can see the “About us” page, which explains the application.</div>
-			<div>6.	The user can see the it’s personal information and modify it.</div>
-			<div>7.	The user logouts.</div>
-		</div>
-		<div class="col-xs-1 col-sm-6 col-lg-6">
-			<h2>Thank you</h2>
-			Thank you for choosing our application!
-		</div>
+			<h2>Know more about our application!</h2>
+			<img src="/img/1.PNG" width="720" height="700"></>
+			<img src="/img/2.PNG" width="720" height="700"></>
+			<img src="/img/3.PNG" width="720" height="500"></>
 	</div>
 <?php 
 	include 'footer.html.php';
